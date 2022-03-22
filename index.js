1e9d3feada12a6b9a7f575759caa3b072aebc909
Angely Granados <angelinagranados@gmail.com>
2022-03-22 18:31:46 -0500

    creting initial configurations

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const { config } = require("./config/index");
+//const usersApi = require("./routes/users.js");
+//const usersTasksApi = require("./routes/user_tasks.js");
+
+//bodyparser
+app.use(express.json());
+app.use(cors());
+//usersApi(app);
+//usersTasksApi(app);
+
+app.listen(config.port, function () {
+  console.log(`Listening http://localhost:${config.port}`);
+});
